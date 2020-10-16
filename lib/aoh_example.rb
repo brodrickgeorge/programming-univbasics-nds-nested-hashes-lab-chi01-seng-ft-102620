7c5c10a13184b6eaf2f5efec089eda6ef06c9d02
Brodrick George <brodrick.george@yahoo.com>
2020-10-16 09:19:54 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -10,6 +10,7 @@ def assembled_aoh
   people = [{ name:  "Don Gately", occupation: "Live-in Staff" },
   { name: "Joelle van Dyne", occupation: "Radio Personality" }
   { name:  "Pat Monteseian", occupation: "Staff" }
+  { name:  "Kate Gompert", occupation: "None" }
   
   # Build an array that contains (or, "nests") the constants into a single
   # Array. Ruby constants are denoted by ALL_CAPS
