b1e14a255e4dddc7a49e502e676c6ce5b1dcbd6e
Brodrick George <brodrick.george@yahoo.com>
2020-10-16 09:18:56 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -7,7 +7,7 @@ KATE_G = { name:  "Kate Gompert", occupation: "None" }
 BRUCE_G = { name:  "Bruce Green", occupation: "Fan of Mildred" }
 
 def assembled_aoh
-  people = []
+  people = [{ name:  "Don Gately", occupation: "Live-in Staff" },
   
   
   # Build an array that contains (or, "nests") the constants into a single
